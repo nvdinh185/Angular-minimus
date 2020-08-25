1278c44850603954c4c19a7c40fb899aee2216ce
nvdinh185 <nvdinh0766777123@gmail.com>
2020-08-25 08:50:27 +0700

    edit 2 file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: false,
   config: {
     appID: '1f481fd4fafd2cf9425a51599c8abd7a',
-    // apiKey: 'AIzaSyCN1KjTNyB20fE3ERrCWYmA103Ng6ZdZ1A',
     baseURL: 'https://api.openweathermap.org/data/2.5/'
   }
 };
