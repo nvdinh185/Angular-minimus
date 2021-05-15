c6a71298ed6081eec82ada29dd15ab1bf0e23179
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-05-15 10:10:53 +0700

    review

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/details/details.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
     { path: 'details/:city', component: DetailsComponent },
 ];
 
