22ccf211360c57ae53dc19b181a5acba7790ee78
nvdinh185 <nvdinh0766777123@gmail.com>
2022-06-03 08:24:23 +0700

    review code

--- angular-build/polyfills.js
+++ angular-build/polyfills.js
@@ -5788,8 +5788,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\DINHNV\MyData\LapTrinhDiDong\Angular\angular-minimus\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! D:\DINHNV\MyData\LapTrinhDiDong\Angular\angular-minimus\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! E:\MYDATA\LAPTRINH\ANGULAR\angular-minimus\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! E:\MYDATA\LAPTRINH\ANGULAR\angular-minimus\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
